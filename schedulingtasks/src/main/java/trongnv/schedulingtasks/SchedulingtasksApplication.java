b46488e940177fd9d5e6f97724076bdc1cfdddd7
Nguyen Van Trong <trongnguyenvann@gmail.com>
2022-08-11 00:22:54 +0700

    Add scheduling task in Spring

---
+++ schedulingtasks/src/main/java/trongnv/schedulingtasks/SchedulingtasksApplication.java
@@ -0,0 +1,15 @@
+package trongnv.schedulingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class SchedulingtasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingtasksApplication.class, args);
+	}
+
+}
---
+++ schedulingtasks/src/test/java/trongnv/schedulingtasks/SchedulingtasksApplicationTests.java
@@ -0,0 +1,13 @@
+package trongnv.schedulingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingtasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
